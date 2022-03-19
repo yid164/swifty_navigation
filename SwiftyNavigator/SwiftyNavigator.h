f9fea665ffd99d8446eddcba4e9163b7d7ca51df
Ken Dong <yid164@mail.usask.ca>
2022-03-18 18:13:14 -0600

    Initial Commit

---
+++ SwiftyNavigator/SwiftyNavigator.h
@@ -0,0 +1,18 @@
+//
+//  SwiftyNavigator.h
+//  SwiftyNavigator
+//
+//  Created by Yinsheng Dong on 2022-03-18.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftyNavigator.
+FOUNDATION_EXPORT double SwiftyNavigatorVersionNumber;
+
+//! Project version string for SwiftyNavigator.
+FOUNDATION_EXPORT const unsigned char SwiftyNavigatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyNavigator/PublicHeader.h>
+
+
